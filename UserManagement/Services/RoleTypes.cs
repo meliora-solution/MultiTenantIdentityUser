0e346416fff0ff94b5c6e8a2eef07547401a942e
hermawih Hasan <hermawih@hotmail.co.id>
2024-02-05 09:37:33 +0700

    Admin Role update

---
+++ UserManagement/Services/RoleTypes.cs
@@ -0,0 +1,24 @@
+﻿using AuthPermissions.BaseCode.DataLayer.Classes.SupportTypes;
+using System.Linq.Expressions;
+
+namespace UserManagement.Services
+{
+    public static class RoleTypesExtensions
+    {
+        public static IQueryable<RoleTypes> AsQueryable(this Enum enumValue)
+        {
+            var type = enumValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("Argument is not an Enum");
+            }
+
+            var values = Enum.GetValues(type).OfType<Enum>();
+            var parameter = Expression.Parameter(type, "x");
+            var memberExpression = Expression.Convert(parameter, typeof(RoleTypes));
+            var lambda = Expression.Lambda<Func<Enum, RoleTypes>>(memberExpression, parameter);
+
+            return values.Select(lambda.Compile()).AsQueryable();
+        }
+    }
+}
